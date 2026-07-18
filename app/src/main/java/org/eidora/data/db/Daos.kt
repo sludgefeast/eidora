788367f2da512b36a9c42190e412522e480163fb
Sebastian Patschorke <physicspatschi@gmx.de>
2026-07-19 00:49:08 +0200

    add missing import

--- app/src/main/java/org/eidora/data/db/Daos.kt
+++ app/src/main/java/org/eidora/data/db/Daos.kt
@@ -2,6 +2,7 @@ package org.eidora.data.db
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
